0d564721ab3eff1603cbbb618aa9a1677b21029a
ArsalaBangash <arsalabangash@gmail.com>
2020-08-11 17:15:23 -0400

    Setup basic scaffolding for the cssc website. Content works, and 3 global components have been setup

---
+++ cssc/components/global/Grid2x3.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row">
+    <div v-for="child in children" :key="child.title" class="col-sm-6 col-lg-4 pr-sm-4 pr-lg-5 mt-5 mb-4">
+      <ThreeColumnChild :title="child.title" :desc="child.desc" :icon="child.icon" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    children: Array
+  }
+}
+</script>
---
+++ cssc/pages/events/_slug.vue
@@ -0,0 +1,25 @@
+<template>
+  <article class="mt-5 pb-5">
+    <nuxt-content
+      class="prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto"
+      :document="page"
+    />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content, params, error }) {
+    console.log(params.slug)
+    const page = await $content('events', params.slug).fetch()
+
+    return { page }
+  },
+  methods: {
+    formatDate (date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleDateString('en', options)
+    }
+  }
+}
+</script>
