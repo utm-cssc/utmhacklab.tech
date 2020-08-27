94ab2985fea95c11dad62999267021ec6034bf79
Zain Kazmi <zkazmi786@hotmail.ca>
2020-08-27 11:21:33 -0400

    Add Anonymousquestion vue file

---
+++ cssc/pages/resources/anonymousquestions.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1> Ask an Upper Year! </h1>
+      <input type="checkbox"></input>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+~                                                                                                                       ~                                                                                                                       ~                             
