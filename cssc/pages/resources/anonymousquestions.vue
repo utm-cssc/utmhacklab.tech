1aa84804dff65cadcffbccaabc015a07ccf3afa5
Zain Kazmi <zkazmi786@hotmail.ca>
2020-08-27 12:09:12 -0400

    Format Anon question form V1

--- cssc/pages/resources/anonymousquestions.vue
+++ cssc/pages/resources/anonymousquestions.vue
@@ -1,10 +1,17 @@
 <script>
 /* eslint-disable */
 </script>
+
+<style>
+.content {
+  max-width: 500px;
+  margin: auto;
+}
+</style>
  
 <template>
-  <div>
-    <h1> Ask an Upper Year! </h1>
+  <div class="content">
+    <h1 style="font-size: 50px;"> Ask an Upper Year! </h1>
       <form>
       	<li style="list-style-type: none;"><label for="ask_alex">Ask Alex: TA and POST</label>
       	<input type="checkbox" text="Ask Alex" id="ask_alex"/>
@@ -19,9 +26,27 @@
       	</li>
 	<li style="list-style-type: none;">
       	<label for=Question> Input your Question here </label> 
-      	<textarea id="Question" style="border-style: inset;"></textarea>
+      	</li>
+	<li style="list-style-type: none;">
+	<textarea id="Question" style="border-style: inset;"></textarea>
+	</li>
+	<li style="list-style-type: none;">
+	<h2 style="font-size: 20px;">Tags:</h2>
+	</li>
+	<li style="list-style-type: none;">
+	<label for="post">Post</label>
+	<input type="checkbox" id="post"/>
+	</li>
+	<li style="list-style-type: none;">	
+	<label for="TA_stuff">TA stuff</label>
+	<input type="checkbox" id="TA_stuff"/>
+	</li>
+	<li style="list-style-type: none;">
+	<label for="tech">Technology</label>
+	<input type="checkbox" id="tech"/>
 	</li>
 	<input type="submit" value="Submit">
+
       </form>
 
   </div>
