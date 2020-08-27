92f7e4c5f3846b759d44280b512af84dab445653
Zain Kazmi <zkazmi786@hotmail.ca>
2020-08-27 11:51:15 -0400

    Add unformatted HTML

--- cssc/pages/resources/anonymousquestions.vue
+++ cssc/pages/resources/anonymousquestions.vue
@@ -1,7 +1,29 @@
+<script>
+/* eslint-disable */
+</script>
+ 
 <template>
   <div>
     <h1> Ask an Upper Year! </h1>
-      <input type="checkbox"></input>
+      <form>
+      	<li style="list-style-type: none;"><label for="ask_alex">Ask Alex: TA and POST</label>
+      	<input type="checkbox" text="Ask Alex" id="ask_alex"/>
+      	</li>
+      	<li style="list-style-type: none;">
+      	<label for="ask_jess">Ask Jess</label>
+     	<input type="checkbox" id="ask_jess"/>
+      	</li>
+      	<li style="list-style-type: none;">
+     	<label for="ask_chris">Ask Chris</label>
+      	<input type="checkbox" id="ask_chris" />
+      	</li>
+	<li style="list-style-type: none;">
+      	<label for=Question> Input your Question here </label> 
+      	<textarea id="Question" style="border-style: inset;"></textarea>
+	</li>
+	<input type="submit" value="Submit">
+      </form>
+
   </div>
 </template>
 
